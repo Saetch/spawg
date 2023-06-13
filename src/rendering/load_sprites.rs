cdbd4b61e716af825de5f2427abd85b07253da6f
Saetch <nils.wriedt@hotmail.com>
2023-06-14 01:17:21 +0200

    added stumps for model thread and updated logic somewhat, cleaned some things up

--- src/rendering/load_sprites.rs
+++ src/rendering/load_sprites.rs
@@ -66,148 +66,6 @@ pub fn load_sprites(_i: u32, renderer: &Renderer) -> (RenderPipeline, BindGroup)
             // let wgpu define it.
             let dwarf_base_house_texture_view = diffuse_texture.create_view(&wgpu::TextureViewDescriptor::default());        //create a handle to access the texture we just created
         
-    
-            /*
-            let diffuse_bytes = include_bytes!("../../textures/image_img.png");
-            let diffuse_image = image::load_from_memory(diffuse_bytes).unwrap();
-            let diffuse_rgba = diffuse_image.to_rgba8();
-    
-            let dimensions = diffuse_image.dimensions();
-    
-            let texture_size = wgpu::Extent3d {
-                width: dimensions.0,
-                height: dimensions.1,
-                depth_or_array_layers: 1,
-            };
-    
-            let second_diffuse_texture = device.create_texture(&wgpu::TextureDescriptor {
-    
-                label: Some("distinct texture"),
-                size: texture_size,
-                mip_level_count: 1,
-                sample_count: 1,
-                dimension: wgpu::TextureDimension::D2,
-                format: wgpu::TextureFormat::Rgba8Unorm,
-                usage: TextureUsages::COPY_DST | TextureUsages::COPY_SRC | TextureUsages::TEXTURE_BINDING,
-                
-            });
-            
-            //this execute a write on the gpu from the loaded image pixel data into our created texture
-            queue.write_texture(
-                // Tells wgpu where to copy the pixel data
-                wgpu::ImageCopyTexture {
-                    texture: &second_diffuse_texture,
-                    mip_level: 0,
-                    origin: wgpu::Origin3d::ZERO,
-                    aspect: wgpu::TextureAspect::All,
-                },
-                // The actual pixel data
-                diffuse_rgba.into_raw().as_slice(),
-                // The layout of the texture
-                wgpu::ImageDataLayout {
-                    offset: 0,
-                    bytes_per_row: std::num::NonZeroU32::new(4 * dimensions.0),
-                    rows_per_image: std::num::NonZeroU32::new(dimensions.1),
-                },
-                texture_size,
-            );
-            let rust_logo_diffuse_texture_view = second_diffuse_texture.create_view(&wgpu::TextureViewDescriptor::default());        //create a handle to access the texture we just created
-
-
-            let diffuse_bytes = include_bytes!("../../textures/background1_cracked_floor.png");
-            let diffuse_image = image::load_from_memory(diffuse_bytes).unwrap();
-            let diffuse_rgba = diffuse_image.to_rgba8();
-    
-            let dimensions = diffuse_image.dimensions();
-    
-            let texture_size = wgpu::Extent3d {
-                width: dimensions.0,
-                height: dimensions.1,
-                depth_or_array_layers: 1,
-            };
-    
-            let second_diffuse_texture = device.create_texture(&wgpu::TextureDescriptor {
-    
-                label: Some("distinct texture"),
-                size: texture_size,
-                mip_level_count: 1,
-                sample_count: 1,
-                dimension: wgpu::TextureDimension::D2,
-                format: wgpu::TextureFormat::Rgba8Unorm,
-                usage: TextureUsages::COPY_DST | TextureUsages::COPY_SRC | TextureUsages::TEXTURE_BINDING,
-                
-            });
-            
-
-            //this execute a write on the gpu from the loaded image pixel data into our created texture
-            queue.write_texture(
-                // Tells wgpu where to copy the pixel data
-                wgpu::ImageCopyTexture {
-                    texture: &second_diffuse_texture,
-                    mip_level: 0,
-                    origin: wgpu::Origin3d::ZERO,
-                    aspect: wgpu::TextureAspect::All,
-                },
-                // The actual pixel data
-                diffuse_rgba.into_raw().as_slice(),
-                // The layout of the texture
-                wgpu::ImageDataLayout {
-                    offset: 0,
-                    bytes_per_row: std::num::NonZeroU32::new(4 * dimensions.0),
-                    rows_per_image: std::num::NonZeroU32::new(dimensions.1),
-                },
-                texture_size,
-            );
-            let background1_cracked_floor_texture_view = second_diffuse_texture.create_view(&wgpu::TextureViewDescriptor::default());        //create a handle to access the texture we just created
-
-
-            let diffuse_bytes = include_bytes!("../../textures/background1_cracked_no_floor.png");
-            let diffuse_image = image::load_from_memory(diffuse_bytes).unwrap();
-            let diffuse_rgba = diffuse_image.to_rgba8();
-    
-            let dimensions = diffuse_image.dimensions();
-    
-            let texture_size = wgpu::Extent3d {
-                width: dimensions.0,
-                height: dimensions.1,
-                depth_or_array_layers: 1,
-            };
-    
-            let second_diffuse_texture = device.create_texture(&wgpu::TextureDescriptor {
-    
-                label: Some("distinct texture"),
-                size: texture_size,
-                mip_level_count: 1,
-                sample_count: 1,
-                dimension: wgpu::TextureDimension::D2,
-                format: wgpu::TextureFormat::Rgba8Unorm,
-                usage: TextureUsages::COPY_DST | TextureUsages::COPY_SRC | TextureUsages::TEXTURE_BINDING,
-                
-            });
-            
-            //this execute a write on the gpu from the loaded image pixel data into our created texture
-            queue.write_texture(
-                // Tells wgpu where to copy the pixel data
-                wgpu::ImageCopyTexture {
-                    texture: &second_diffuse_texture,
-                    mip_level: 0,
-                    origin: wgpu::Origin3d::ZERO,
-                    aspect: wgpu::TextureAspect::All,
-                },
-                // The actual pixel data
-                diffuse_rgba.into_raw().as_slice(),
-                // The layout of the texture
-                wgpu::ImageDataLayout {
-                    offset: 0,
-                    bytes_per_row: std::num::NonZeroU32::new(4 * dimensions.0),
-                    rows_per_image: std::num::NonZeroU32::new(dimensions.1),
-                },
-                texture_size,
-            );
-            let background1_cracked_no_floor_texture_view = second_diffuse_texture.create_view(&wgpu::TextureViewDescriptor::default());        //create a handle to access the texture we just created
-
-            
-    
     /*      This is another way to load an image, but it is not as easy to use as the one above (not as flexible), tho more slim
         
             let diffuse_texture = {
@@ -244,8 +102,8 @@ pub fn load_sprites(_i: u32, renderer: &Renderer) -> (RenderPipeline, BindGroup)
                 );
                 texture
             };*/
-    
-            */
+
+            
     
     
                     
