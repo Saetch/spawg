5d482727be87ca6c3f9f819a019ce6c7491315ad
Saetch <nils.wriedt@hotmail.com>
2023-06-14 21:17:34 +0200

    added stumps for handling types

--- src/controller/controller.rs
+++ src/controller/controller.rs
@@ -15,4 +15,22 @@ impl Controller{
             receiver: receiver,
         }
     }
+
+
+    pub(crate) fn run(&mut self){
+
+        let mut personal_running_bool = true;       //we dont need a shared value, as we get notified of a shutdown via the channel
+        while personal_running_bool{
+            let received = self.receiver.recv().unwrap();
+
+            match received{
+                ControllerInput::Exit => {
+                    personal_running_bool = false;
+                }
+                ControllerInput::MouseInput { action } =>  println!("Got mouse input!"),
+                ControllerInput::KeyboardInput { key, state } =>  println!("Got keyboard input!"),
+                ControllerInput::WindowResized { dimensions } =>    println!("Got window resized!"),
+            }
+        }
+    }
 }
