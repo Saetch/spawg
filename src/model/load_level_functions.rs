aac77f922916b657d48305ad25edd27e0d70bb2a
Saetch <nils.wriedt@hotmail.com>
2023-06-25 13:27:53 +0200

    added stumps for lines, which are to debug and to visualize

--- src/model/load_level_functions.rs
+++ src/model/load_level_functions.rs
@@ -1,4 +1,4 @@
-use crate::{game_objects::buildings::debug_house::DebugHouse, rendering::sprites::{sprite_mapping::Sprite, vertex_configration::VertexConfigration}, controller::position::Position};
+use crate::{game_objects::{buildings::debug_house::DebugHouse, debug::line::Line}, rendering::sprites::{sprite_mapping::Sprite, vertex_configration::VertexConfigration}, controller::position::Position};
 
 use super::model::Model;
 
@@ -19,7 +19,6 @@ impl Model{
         let mut lock = self.game_objects.write().await;
         
         lock.clear();
-
 
         //add a debug house, here other stuff is loaded aswell
         lock.push(Box::new(DebugHouse::new(Sprite::DwarfBaseHouse, Position::new(0.0, 0.0), VertexConfigration::SQUARE_SMALL_1)));   //these are debug objects and thus require what texture and size they need, usually objects will silently put these in the constructor or just have const fn for these
