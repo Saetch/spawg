3a8d09e891433f5d7086a5f5c1cc9e09193d1e69
Saetch <nils.wriedt@hotmail.com>
2023-06-29 02:00:23 +0200

    added comments

--- src/model/load_level_functions.rs
+++ src/model/load_level_functions.rs
@@ -34,7 +34,8 @@ impl Model{
         //create a Maze
         let (maze, to_add_objects ) = Maze::new(43, 24, (-10.0, -5.0));
         let background_square = DebugHouse::new(Sprite::DarkBlue, Position { x: 1241.2, y: 1231.1 }, VertexConfigration::NEARLY_SQUARE_RECTANGLE_0);
-        self.add_game_object(Arc::new(RwLock::new(background_square))).await;
+        self.add_game_object(Arc::new(RwLock::new(background_square))).await;  //this is just a workaround. The cam_organizer start at game_objects[0] and goes through all, constructing the necessary commands, which means that the vertex_configurations that are seen first are in the background and the ones later are in the foreground
+        //this can be avoided by implementing a depth buffer to use in the wgpu render-pipeline
         self.add_logic_object(Box::new(maze));
         self.add_game_objects(to_add_objects).await;
 
