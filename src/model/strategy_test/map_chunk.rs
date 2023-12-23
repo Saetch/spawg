7ee611070ab831cabc48c2f6f0c5f226b279c108
Saetch <nils.wriedt@hotmail.com>
2023-12-23 13:59:32 +0100

    added necessary traits for the basic level functionality

--- src/model/strategy_test/map_chunk.rs
+++ src/model/strategy_test/map_chunk.rs
@@ -1,15 +1,18 @@
+use std::fmt;
+
 use crate::controller::position::Position;
 
-
+#[derive(Debug)]
 pub(crate) struct IntEdge{
     x: i32,
     y: i32,
 }
+#[derive(Debug)]
 pub(crate) struct ChunkInfo{
     bottom_left: IntEdge,
     top_right: IntEdge,
 }
-pub(crate) trait MapChunk {
+pub(crate) trait MapChunk: fmt::Debug{
     
     fn identify(&self) -> String;
     fn inf(&self) -> ChunkInfo;
--- src/model/strategy_test/strategy_test.rs
+++ src/model/strategy_test/strategy_test.rs
@@ -1,12 +1,31 @@
-use crate::game_objects::game_object::VisitableStructure;
+use std::time::Duration;
+
+use crate::{game_objects::game_object::{VisitableStructure, LogicObject}, model::results::LogicResult};
 
 use super::map_chunk::{MapChunk, ChunkInfo};
 
 
 //the idea is to hold information about the current level, this includes the chunks that are currently loaded, the structures that are currently loaded and the borders of the level. This is supposed to be used for stuff like pathfinding and collision detection
-pub struct LevelInfo{
+#[derive(Debug)]
+pub struct StratLevel{
     blocking_chunks: Vec<Box<dyn MapChunk>>,
     non_blocking_chunks: Vec<Box<dyn MapChunk>>,
     structures: Vec<Box<dyn VisitableStructure>>,
     borders: ChunkInfo,
+    id: u32,
+}
+
+impl LogicObject for StratLevel{
+    fn process_logic(&mut self, delta_time: Duration) -> LogicResult {
+        LogicResult::None
+    }
+
+    fn set_id(&mut self, id: u32) {
+        self.id = id;
+    }
+
+    fn get_id(&self) -> u32 {
+        self.id
+    }
+    
 }
